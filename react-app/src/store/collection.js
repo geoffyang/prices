c53ededd52d4893431966eeb2eaac72c4e1fa333
geoffyang <geoffreyyang@gmail.com>
2021-07-30 11:57:31 -0400

    collection thunks

--- react-app/src/store/collection.js
+++ react-app/src/store/collection.js
@@ -2,7 +2,7 @@ const SET_COLLECTION = "collection/SET_COLLECTION"
 
 const setCollection = (collections) => ({
     type: SET_COLLECTION,
-    collections
+    collections:collections
 })
 
 export const getCollection = () => async (dispatch) => {
@@ -12,12 +12,30 @@ export const getCollection = () => async (dispatch) => {
         if (data.errors) {
             return;
         }
+        dispatch(setCollection(data))
     }
-    dispatch(setCollection)
+}
 
+export const addCollection = (name, user_id) => async (dispatch) => {
+    const response = await fetch('/api/collections', {
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            name, user_id
+        })
+    })
 }
 
 
+export default function reducer(state = {}, action) {
+    switch (action.type) {
+        case SET_COLLECTION:
+            return { collections: action.collections }
+        default:
+            return state;
 
+    }
 
-const initialState = {};
+}
