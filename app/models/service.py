04c9fee501ae7198963fd094116c30f7b34e739c
geoffyang <geoffreyyang@gmail.com>
2021-08-01 14:27:10 -0400

    add service_collections table

--- app/models/service.py
+++ app/models/service.py
@@ -1,12 +1,12 @@
 from .db import db
 from datetime import datetime
+from .service_collections import service_collections
 
 
 class Service(db.Model):
     __tablename__ = 'services'
-
+    
     id = db.Column(db.Integer, primary_key=True)
-
     billing_code = db.Column(db.Integer, nullable=False)
     cpt_code = db.Column(db.String(10), nullable=True)
     service_description = db.Column(db.String(140), nullable=False)
@@ -14,22 +14,28 @@ class Service(db.Model):
     discounted_price = db.Column(db.NUMERIC(9, 2), nullable=True)
     domain = db.Column(db.String(20), nullable=True)
     subdomain = db.Column(db.String(20), nullable=True)
-
-    # FK
-    hospital_id = db.Column(db.Integer,
-                            db.ForeignKey("hospitals.id",
-                                          ondelete='CASCADE'),
-                            nullable=False
-                            )
+    hospital_id = db.Column(
+        db.Integer,
+        db.ForeignKey("hospitals.id", ondelete='CASCADE'),
+        nullable=False
+    )
     # user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
     status = db.Column(db.String(20))
-    created_at = db.Column(db.DateTime,
-                           nullable=False,
-                           default=datetime.utcnow)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
 
-    #relationships
+    # relationships
     hospital = db.relationship('Hospital', backref=db.backref(
         "services", passive_deletes=True))
+    collections = db.relationship(
+        "Collection",
+        secondary=service_collections,
+        back_populates="services"
+    )
+
 
     def to_dict(self):
         return{
