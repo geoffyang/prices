b252cd42ccc9a174d4091d44d1b06eae3662b8eb
geoffyang <geoffreyyang@gmail.com>
2021-08-05 16:42:16 -0400

    about

--- react-app/src/components/ServicePage.js
+++ react-app/src/components/ServicePage.js
@@ -24,27 +24,49 @@ export default function ServicePage() {
             <div>
 
                 <div id="service__top">
-                    <div id="breadcrumbs">Home / </div>
-                    <div className="labels"> Service number
-                        <span className="values">{id}</span></div>
-                    <div className="labels"> Billing Code
-                        <span className="values">{s.billing_code}</span></div>
-                    <div className="labels"> CPT Code
-                        <span className="values">{s.cpt_code}</span></div>
-                    <div className="labels"> Service Description
-                        <span className="values">{s.service_description}</span></div>
-                    <div className="labels"> List Price
-                        <span className="values">{s.list_price.toFixed(2)}</span></div>
-                    <div className="labels"> Discounted Price
-                        <span className="values">{s.discounted_price.toFixed(2)}</span></div>
-                </div>
+                    <div id="breadcrumbs">Services</div>
+                    <div id="service-name">
+                        <div id="service-description" >{s.service_description}
+                        </div>
+                        <div id="billing-code" className="labels"> Billing Code
+                            <span id="service-description-code" >{s.billing_code}</span>
+                        </div>
+                    </div>
+
+                    <div id="service__about" className="titles">
+                        <div id="service__about__left" className="service__left-column">About</div>
+
+                        <div className="service__middle-spacer"></div>
+
+                        <div id="service__about__right" className="service__right-column">
+
+                            <div className=" service-grid-stats"> List Price
+                                <div className="service__grid-values">${s.list_price.toFixed(2)}</div>
+                            </div>
+
+                            <div className=" service-grid-stats"> Discounted Price
+                                <div className="service__grid-values">${s.discounted_price.toFixed(2)}</div>
+                            </div>
+
+                            <div className=" service-grid-stats"> CPT Code
+                                <div className="service__grid-values">{s.cpt_code}</div>
+                            </div>
+
+                            <div className=" service-grid-stats"> Hospital ID
+                                <div className="service__grid-values">{s.hospital_id}</div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div >
                 <div id="comments-form">
                     <CommentsForm />
                 </div>
                 <div id="service__bottom">
                     <Comments />
                 </div>
-            </div>
+            </div >
         )
         : <h3>That service doesn't exist</h3>
     )
